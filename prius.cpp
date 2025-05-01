647389c930d1bec3faafdf5b131cf29c77d39cb5
Iskender-03 <guliyev2003@gmail.com>
2025-05-01 12:44:45 +0400

    Update prius.cpp

--- prius.cpp
+++ prius.cpp
@@ -11,7 +11,7 @@ Prius::Prius(QGraphicsItem *parent)
     turboActive(false)
 {
 
-    QPixmap pixmap("C:/Users/iskender/Documents/Crazy_Prius_Simulator/pictures/prius2.jpg");
+    QPixmap pixmap(":/pictures/prius2.jpg");
     if (pixmap.isNull()) {
         qDebug() << "Error: Failed to load prius.jpg";
         pixmap = QPixmap(30, 50);
